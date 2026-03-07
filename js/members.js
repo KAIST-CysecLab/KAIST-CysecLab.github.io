7b64d9010f21228d44f42ac6a47d6a69f9664eec
Hyeonwoo Kang <kanglib14@gmail.com>
2026-03-07 22:33:54 +0900

    Refactor members section (1)

---
+++ js/members.js
@@ -0,0 +1,342 @@
+// name email [photo] [homepage]
+const phd_students = [
+  {
+    name: "Hyeonwoo Kang",
+    email: "kanglib",
+    // homepage: "https://kanglib.pe.kr/",
+  },
+  {
+    name: "Chang-il Lim",
+    email: "notavirus1",
+  },
+  {
+    name: "Eunjin Kim",
+    email: "ej.kim20",
+  },
+  {
+    name: "Sangyoon Kim",
+    email: "ksy980418",
+  },
+  {
+    name: "Donghwa Kang",
+    email: "anima0729",
+    homepage: "https://github.com/anima0729",
+  },
+  {
+    name: "Byeongsu Woo",
+    email: "wbs79",
+    photo: "wbs.jpg",
+    homepage: "https://mysigyeong.github.io/",
+  },
+  {
+    name: "Seo-ye Kim",
+    email: "kimseoye",
+  },
+  {
+    name: "Doohyun Kim",
+    email: "doohkim7",
+  },
+  {
+    name: "ChanU Kang",
+    email: "hle4s",
+  },
+  {
+    name: "Hojun Choe",
+    email: "choehojun",
+  },
+];
+
+// name email [photo] [homepage]
+const ms_students = [
+  {
+    name: "Jongmin Kim",
+    email: "slyfizz",
+    homepage: "https://slyfizz3.github.io/about/",
+  },
+  {
+    name: "Mingyu Jun",
+    email: "mingyu.jun",
+  },
+  {
+    name: "Hyunseok Kim",
+    email: "ubuntu",
+  },
+];
+
+// name desc1 [desc2] [photo]
+const alumni = [
+  {
+    name: "Sunjune Kong",
+    desc1: "M.S. Graduate in 2015",
+    desc2: "Samsung Electronics",
+    photo: "ksj.gif",
+  },
+  {
+    name: "Seunghyeon Lee",
+    desc1: "M.S. Graduate in 2015",
+    desc2: "S2W",
+    photo: "lsh.gif",
+  },
+  {
+    name: "Minsu Kim",
+    desc1: "Ph.D. Graduate in 2018",
+    desc2: "S2W",
+    photo: "minsukim.jpg",
+  },
+  {
+    name: "Daehee Jang",
+    desc1: "Ph.D. Graduate in 2019",
+    desc2: "Professor at Kyung Hee University",
+    photo: "jdh.jpg",
+  },
+  {
+    name: "Jinsoo Jang",
+    desc1: "Ph.D. Graduate in 2019",
+    desc2: "Professor at Chungnam National University",
+    photo: "jjs.jpg",
+  },
+  {
+    name: "Deokjin Kim",
+    desc1: "Ph.D. Graduate in 2019",
+    desc2: "National Security Research Institute (NSR)",
+  },
+  {
+    name: "Hojoon Lee",
+    desc1: "Ph.D. Graduate in 2019",
+    desc2: "Professor at Sungkyunkwan University",
+    photo: "lhj.jpg",
+  },
+  {
+    name: "Jiseong Noh",
+    desc1: "Ph.D. Graduate in 2019",
+    desc2: "National Security Research Institute (NSR)",
+    photo: "njs.jpg",
+  },
+  {
+    name: "Sejin Jeong",
+    desc1: "M.S. Graduate in 2019",
+    desc2: "Korea Army",
+  },
+  {
+    name: "Soohun Kim",
+    desc1: "M.S. Graduate in 2019",
+    desc2: "National Security Research Institute (NSR)",
+    photo: "ksh.jpeg",
+  },
+  {
+    name: "Seokjoo Mun",
+    desc1: "M.S. Graduate in 2019",
+    desc2: "LG",
+  },
+  {
+    name: "Changho Choi",
+    desc1: "Ph.D. Graduate in 2020",
+    desc2: "Samsung Research",
+    photo: "cch2.jpg",
+  },
+  {
+    name: "Dongjae Jung",
+    desc1: "Ph.D. Graduate in 2020",
+    desc2: "KAIST Cyber Security Research Center",
+    photo: "jdj.jpg",
+  },
+  {
+    name: "Jonghwan Kim",
+    desc1: "Ph.D. Graduate in 2020",
+    desc2: "LINE, Japan",
+    photo: "kjh.jpg",
+  },
+  {
+    name: "Suyeon Yoo",
+    desc1: "Ph.D. Graduate in 2020",
+    desc2: "National Security Research Institute (NSR)",
+    photo: "syyoo.jpg",
+  },
+  {
+    name: "Seungyeop Lee",
+    desc1: "M.S. Graduate in 2020",
+    desc2: "Ground X",
+  },
+  {
+    name: "Jaehyuk Lee",
+    desc1: "Ph.D. Graduate in 2021",
+    desc2: "Postdoc fellow at Georgia Tech",
+    photo: "ljh120x150-165120x150-165.jpg",
+  },
+  {
+    name: "Wonwoo Choi",
+    desc1: "M.S. Graduate in 2022",
+    desc2: "National Security Research Institute (NSR)",
+  },
+  {
+    name: "Hyunsoo Shin",
+    desc1: "M.S. Graduate in 2022",
+    desc2: "Ground X",
+  },
+  {
+    name: "Yeseul Choi",
+    desc1: "Ph.D. Graduate in 2023",
+    desc2: "Electronics and Telecommunications Research Institute (ETRI)",
+    photo: "ces120x150-141.jpg",
+  },
+  {
+    name: "Youngkwang Han",
+    desc1: "Ph.D. Graduate in 2023",
+    desc2: "Postdoc fellow at KAIST",
+    photo: "youngkwang.jpeg",
+  },
+  {
+    name: "Daegyeong Kim",
+    desc1: "Ph.D. Graduate in 2023",
+    desc2: "Postdoc fellow at KAIST",
+    photo: "kgd.gif",
+  },
+  {
+    name: "Seongman Lee",
+    desc1: "Ph.D. Graduate in 2023",
+    desc2: "Postdoc fellow at KAIST",
+  },
+  {
+    name: "Junsu Lee",
+    desc1: "M.S. Graduate in 2023",
+    desc2: "Theori",
+  },
+  {
+    name: "Taehyeon Lee",
+    desc1: "M.S. Graduate in 2023",
+    desc2: "National Security Research Institute (NSR)",
+  },
+  {
+    name: "Seungyong Yang",
+    desc1: "Ph.D. Graduate in 2025",
+    desc2: "S2W",
+    photo: "ysy.jpg",
+  },
+  {
+    name: "Yongho Song",
+    desc1: "Ph.D. Graduate in 2025",
+    desc2: "CysecuLab",
+    photo: "yhsong.png",
+  },
+  {
+    name: "Eunyeong Ahn",
+    desc1: "M.S. Graduate in 2025",
+    desc2: "National Security Research Institute (NSR)",
+  },
+  {
+    name: "Hyeonsu Lee",
+    desc1: "M.S. Graduate in 2025",
+    desc2: "CysecuLab",
+  },
+  {
+    name: "Jong Heon Lee",
+    desc1: "M.S. Graduate in 2025",
+    desc2: "Samsung SDS",
+  },
+  {
+    name: "Sungkwang Ji",
+    desc1: "M.S. Graduate in 2026",
+    desc2: "National Security Research Institute (NSR)",
+  },
+  {
+    name: "Kihwan Kim",
+    desc1: "M.S. Graduate in 2013",
+    desc2: "LG",
+    photo: "kkh.jpg",
+  },
+  {
+    name: "Kuenwhee Oh",
+    desc1: "M.S. Graduate in 2017",
+    photo: "okw120x150-131.png",
+  },
+  {
+    name: "Chihyun Song",
+    desc1: "M.S. Graduate in 2019",
+    desc2: "Ground X",
+    photo: "chsong120x150.jpg",
+  },
+  {
+    name: "Yunjong Jeong",
+    desc1: "M.S. Graduate in 2020",
+    desc2: "Ground X",
+    photo: "yunjong.jpg",
+  },
+  {
+    name: "Nohyun Kwak",
+    desc1: "Ground X",
+    photo: "knh120x150-131120x150-141.jpg",
+  },
+  {
+    name: "Minjoon Park",
+    desc1: "M.S. Graduate in 2018",
+    desc2: "CysecuLab",
+    photo: "mjpark120x150-141.png",
+  },
+  {
+    name: "Geonwoo Kim",
+    desc1: "M.S. Graduate in 2018",
+    desc2: "CysecuLab",
+    photo: "Gunwoo.jpg",
+  },
+  {
+    name: "Changjo Yun",
+    desc1: "M.S. Graduate in 2023",
+    desc2: "CysecuLab",
+  },
+];
+
+document.querySelector("#phd-students").innerHTML += `${
+  phd_students.map(p => `
+    <div>
+      <div class="thumbnail">
+        <img class="img-circle" src="images/${p.photo || "unknown120x150.jpeg"}">
+        <div class="caption">
+          <h3></h3><p></p><p></p>
+          <h3>${p.name}</h3>
+          <p>Ph.D. Student</p>
+          <p>${p.email} (at) kaist.ac.kr</p>
+          <p></p>
+          <p class="group">
+            <a class="btn btn-default${p.homepage ? "" : " invisible"}" href="${p.homepage}">Homepage</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  `).join("")
+}`;
+
+document.querySelector("#ms-students").innerHTML += `${
+  ms_students.map(p => `
+    <div>
+      <div class="thumbnail">
+        <img class="img-circle" src="images/${p.photo || "unknown120x150.jpeg"}">
+        <div class="caption">
+          <h3></h3><p></p><p></p>
+          <h3>${p.name}</h3>
+          <p>M.S. Student</p>
+          <p>${p.email} (at) kaist.ac.kr</p>
+          <p></p>
+          <p class="group">
+            <a class="btn btn-default${p.homepage ? "" : " invisible"}" href="${p.homepage}">Homepage</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  `).join("")
+}`;
+
+document.querySelector("#alumni").innerHTML += `${
+  alumni.map(p => `
+    <div>
+      <div class="thumbnail">
+        <img class="img-circle" src="images/${p.photo || "unknown120x150.jpeg"}">
+        <div class="caption">
+          <h3></h3><p></p><p></p>
+          <h3>${p.name}</h3>
+          <p>${p.desc1}</p>
+          <p>${p.desc2 || ""}</p>
+        </div>
+      </div>
+    </div>
+  `).join("")
+}`;
